4849076b0caaa27111d0671a675be83783927be6
Miya Chen <miyatlchen@gmail.com>
2020-02-10 15:06:37 +0800

    core, core/rawdb, ethdb: add transfer logs in freezer
    # Conflicts:
    #	core/rawdb/ancient_scheme.go
    #	core/rawdb/database.go

--- core/rawdb/ancient_scheme.go
+++ core/rawdb/ancient_scheme.go
@@ -32,6 +32,9 @@ const (
 
 	// ChainFreezerDifficultyTable indicates the name of the freezer total difficulty table.
 	ChainFreezerDifficultyTable = "diffs"
+
+	// ChainFreezerTransferLogTable indicates the name of the freezer transfer logs table.
+	ChainFreezerTransferLogTable = "transfers"
 )
 
 // chainFreezerNoSnappy configures whether compression is disabled for the ancient-tables.
@@ -42,6 +45,8 @@ var chainFreezerNoSnappy = map[string]bool{
 	ChainFreezerBodiesTable:     false,
 	ChainFreezerReceiptTable:    false,
 	ChainFreezerDifficultyTable: true,
+
+	ChainFreezerTransferLogTable: false,
 }
 
 // The list of identifiers of ancient stores.
