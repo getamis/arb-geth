e7cec2e4ab6cb70d166d22d226f1d433d1c9ef58
Miya Chen <miyatlchen@gmail.com>
2020-02-11 13:35:31 +0800

    core, eth: refactor read/write transfer logs
    # Conflicts:
    #	core/blockchain.go
    
    # Conflicts:
    #	eth/api.go

--- eth/api_debug.go
+++ eth/api_debug.go
@@ -205,6 +205,14 @@ func (api *DebugAPI) AccountRange(blockNrOrHash rpc.BlockNumberOrHash, start hex
 	return stateDb.IteratorDump(opts), nil
 }
 
+// GetTransferLogs is a debug API function that returns the transfer logs for a block hash, if known.
+func (api *DebugAPI) GetTransferLogs(ctx context.Context, hash common.Hash) ([]*types.TransferLog, error) {
+	if transferLogs := api.eth.blockchain.GetTransferLogs(hash); transferLogs != nil {
+		return transferLogs, nil
+	}
+	return nil, errors.New("unknown transfer logs")
+}
+
 // StorageRangeResult is the result of a debug_storageRangeAt API call.
 type StorageRangeResult struct {
 	Storage storageMap   `json:"storage"`
